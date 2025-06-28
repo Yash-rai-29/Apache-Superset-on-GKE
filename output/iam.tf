9d6ff77c424f8c368b938fb9807a521a3e2b47dc
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-28 11:46:17 +0530

    Initial commit

--- output/iam.tf
+++ output/iam.tf
@@ -1,15 +1,18 @@
-data "google_project" "project" {
-  project_id = var.project_id
+data "google_project_iam_policy" "project" {
+  provider = google
+  project = var.project_id
 }
 
-resource "google_project_iam_member" "service_account_token_creator" {
-  project = data.google_project.project.project_id
-  role    = "roles/iam.serviceAccountTokenCreator"
-  member  = "allUsers"
+resource "google_project_iam_member" "no_service_account_user" {
+  provider = google
+  project = var.project_id
+  role   = "roles/iam.serviceAccountUser"
+  member = "allUsers"
 }
 
-resource "google_project_iam_member" "service_account_user" {
-  project = data.google_project.project.project_id
-  role    = "roles/iam.serviceAccountUser"
-  member  = "allUsers"
+resource "google_project_iam_member" "no_service_account_token_creator" {
+  provider = google
+  project = var.project_id
+  role   = "roles/iam.serviceAccountTokenCreator"
+  member = "allUsers"
 }
