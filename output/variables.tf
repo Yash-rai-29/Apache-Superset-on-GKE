1a4598ab761a64356edb7fb8d5ef8ad16f39757b
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-28 11:45:46 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -1,11 +1,17 @@
 variable "project_id" {
   type        = string
-  description = "The GCP project ID"
+  description = "The ID of the project in which to configure resources."
   default     = "aviato-game-fight-rvxirf"
 }
 
-variable "regions" {
-  type    = list(string)
+variable "region" {
+  type        = string
+  description = "The region in which to create resources."
+  default     = "us-central1"
+}
+
+variable "default_subnets" {
+  type = list(string)
   default = [
     "africa-south1",
     "southamerica-west1",
@@ -48,17 +54,12 @@ variable "regions" {
     "europe-west3",
     "us-east1",
     "europe-central2",
-    "me-west1"
+    "me-west1",
   ]
-  description = "List of regions"
 }
 
-variable "bucket_names" {
-  type = list(string)
-  default = [
-    "aviato-game-fight-rvxirf.appspot.com",
-    "aviato-game-fight-rvxirf_bucket",
-    "staging.aviato-game-fight-rvxirf.appspot.com"
-  ]
-  description = "List of cloud storage bucket names"
+variable "api_key_rotation_days" {
+  type        = number
+  description = "Number of days after which API keys should be rotated"
+  default     = 90
 }
