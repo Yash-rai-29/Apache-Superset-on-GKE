703e390bb117b3dc4cb9443c75607e2e5f4d1c35
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-28 11:45:47 +0530

    Initial commit

--- output/main.tf
+++ output/main.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.68.0"
+      version = ">= 4.0"
     }
   }
 }
 
 provider "google" {
   project = var.project_id
+  region  = var.region
 }
 
 resource "google_project_service_identity" "artifactregistry" {
@@ -19,73 +20,76 @@ resource "google_project_service_identity" "artifactregistry" {
 
 resource "google_artifact_registry_repository" "default" {
   provider = google
-  location = "us-central1"
-  repository_id = "default"
+  project      = var.project_id
+  location     = var.region
+  repository_id = "default-repository"
+  format       = "DOCKER"
+}
+
+resource "google_project_iam_binding" "artifactregistry" {
+  provider = google
   project = var.project_id
-  format = "DOCKER"
+  role    = "roles/artifactregistry.reader"
+
+  members = [
+    "serviceAccount:${google_project_service_identity.artifactregistry.email}",
+  ]
+  depends_on = [google_project_service_identity.artifactregistry, google_artifact_registry_repository.default]
+}
+
+
+resource "google_container_analysis_occurrence" "default" {
+  provider = google
+  project = var.project_id
+  note                 = "projects/goog-analysis/notes/PACKAGE_VULNERABILITY"
+  resource_uri         = "https://gcr.io/google-containers/node-hello@sha256:a19856d9e4de32a1d139f835ebc0a9f1245c7e541b4f46c92d59697129b88db9"
+}
+
+
+resource "google_project_service" "artifactregistry" {
+  provider = google
+  project  = var.project_id
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "containeranalysis" {
   provider = google
-  project            = var.project_id
+  project  = var.project_id
   service            = "containeranalysis.googleapis.com"
   disable_on_destroy = false
-}
-
-resource "google_project_iam_member" "containeranalysis" {
-  provider = google
-  project = var.project_id
-  role = "roles/containeranalysis.notes.occurrences.viewer"
-  member = "serviceAccount:${google_project_service_identity.artifactregistry.email}"
+  depends_on = [google_project_service.artifactregistry]
 }
 
 resource "google_project_service" "cloudasset" {
   provider = google
-  project = var.project_id
-  service = "cloudasset.googleapis.com"
+  project  = var.project_id
+  service            = "cloudasset.googleapis.com"
   disable_on_destroy = false
 }
 
+
+resource "google_project_iam_member" "cloudasset" {
+  provider = google
+  project = var.project_id
+  role   = "roles/cloudasset.viewer"
+  member = "allUsers"
+
+  depends_on = [google_project_service.cloudasset]
+}
+
 resource "google_compute_network" "default" {
+  provider = google
   name                    = "default"
   project                 = var.project_id
   delete_default_routes = true
-}
-
-resource "google_compute_network" "default_delete" {
-  name                    = "default"
-  project                 = var.project_id
-  depends_on = [
-    google_compute_network.default
-  ]
-  delete_default_routes = true
-  lifecycle {
-    ignore_changes = [
-      delete_default_routes
-    ]
-    create_before_destroy = true
-  }
-}
-
-resource "google_compute_network" "default_dns" {
-  name                    = "default"
-  project                 = var.project_id
-  enable_dns_logging = true
-  depends_on = [
-    google_compute_network.default
-  ]
-  lifecycle {
-    ignore_changes = [
-      enable_dns_logging
-    ]
-    create_before_destroy = true
-  }
 }
 
 resource "google_compute_firewall" "rdp" {
+  provider = google
   name    = "default-allow-rdp"
-  project = var.project_id
   network = "default"
+  project = var.project_id
 
   allow {
     protocol = "tcp"
@@ -93,14 +97,15 @@ resource "google_compute_firewall" "rdp" {
   }
 
   source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-  priority      = 65534
+
+  target_tags = ["rdp"]
 }
 
 resource "google_compute_firewall" "ssh" {
+  provider = google
   name    = "default-allow-ssh"
-  project = var.project_id
   network = "default"
+  project = var.project_id
 
   allow {
     protocol = "tcp"
@@ -108,50 +113,88 @@ resource "google_compute_firewall" "ssh" {
   }
 
   source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-  priority      = 65534
+
+  target_tags = ["ssh"]
 }
 
-resource "google_compute_subnet" "default_flow_logs" {
-  for_each = toset(var.regions)
-  name                     = "default"
-  project                  = var.project_id
-  region                   = each.key
-  network                  = "default"
-  ip_cidr_range            = "10.128.0.0/20"
-  private_ip_google_access = true
+resource "google_project_default_network_tier" "default" {
+  provider = google
+  project = var.project_id
+  network_tier = "PREMIUM"
+}
+
+resource "google_compute_network" "vpc_network" {
+  provider = google
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+  project                 = var.project_id
+}
+
+resource "google_compute_subnetwork" "default" {
+  provider = google
+  for_each     = toset(var.default_subnets)
+  name         = each.key
+  ip_cidr_range = "10.10.10.0/24"
+  network      = google_compute_network.vpc_network.id
+  region       = each.key
+  project      = var.project_id
+
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 0.5
     metadata             = "INCLUDE_ALL_METADATA"
   }
-  depends_on = [
-    google_compute_network.default
-  ]
 }
 
-resource "google_project_metadata" "oslogin" {
+resource "google_project_default_service_accounts" "default_accounts" {
+  provider = google
+  project     = var.project_id
+
+  action = "DISABLE"
+}
+
+resource "google_project_metadata" "metadata" {
+  provider = google
   project = var.project_id
 
   metadata = {
-    enable-oslogin = "TRUE"
+    enable-oslogin = "true"
   }
 }
 
-resource "google_storage_bucket" "buckets" {
-  for_each = toset(var.bucket_names)
-  name          = each.key
-  project       = var.project_id
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  provider = google
+  project = var.project_id
+  cloud_function = "function"
+  region = "us-central1"
+  role = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "google_logging_project_sink" "default" {
+  provider = google
+  name = "default-sink"
+  project = var.project_id
+  description = "Exports all logs to a Cloud Storage bucket"
+  destination = "storage.googleapis.com/${var.project_id}-logs"
+  filter = "NOT logName:projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity"
+
+  unique_writer_identity = true
+}
+
+resource "google_storage_bucket" "bucket" {
+  provider = google
+  name          = "${var.project_id}-logs"
+  project     = var.project_id
   location      = "US"
   uniform_bucket_level_access = true
 }
 
-resource "google_logging_project_sink" "default" {
-  name        = "all-logs"
-  project     = var.project_id
-  description = "A sink that exports copies of all log entries in the project."
-  destination = "storage.googleapis.com/${var.project_id}-all-logs"
-  filter      = "NOT logName:projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Fdata_access"
-
-  writer_identity = "serviceAccount:gcp-sa-logging@${var.project_id}.iam.gserviceaccount.com"
+resource "google_project_iam_binding" "bucket_access" {
+  provider = google
+  project = var.project_id
+  role   = "roles/storage.objectCreator"
+  members = [
+    "serviceAccount:${google_logging_project_sink.default.writer_identity}",
+  ]
 }
