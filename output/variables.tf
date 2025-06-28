5e4c11056e3398c7be670e836ffe7d7951aee819
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-28 11:06:51 +0530

    Initial commit

---
+++ output/variables.tf
@@ -0,0 +1,71 @@
+variable "project_id" {
+  type = string
+  default = "aviato-game-fight-rvxirf"
+}
+
+variable "regions" {
+  type = list(string)
+  default = [
+    "africa-south1",
+    "southamerica-west1",
+    "us-west4",
+    "me-central2",
+    "asia-east2",
+    "asia-northeast1",
+    "asia-south2",
+    "asia-northeast3",
+    "us-south1",
+    "australia-southeast1",
+    "us-east4",
+    "us-west3",
+    "asia-southeast1",
+    "europe-southwest1",
+    "asia-east1",
+    "europe-north2",
+    "australia-southeast2",
+    "northamerica-northeast2",
+    "asia-southeast2",
+    "northamerica-northeast1",
+    "asia-south1",
+    "europe-west6",
+    "europe-west1",
+    "southamerica-east1",
+    "asia-northeast2",
+    "europe-north1",
+    "us-east5",
+    "us-west2",
+    "us-west1",
+    "europe-west10",
+    "us-central1",
+    "northamerica-south1",
+    "europe-west4",
+    "europe-west9",
+    "europe-west2",
+    "europe-west12",
+    "europe-west8",
+    "me-central1",
+    "europe-west3",
+    "us-east1",
+    "europe-central2",
+    "me-west1"
+  ]
+}
+
+variable "default_rdp_ssh_firewall_name" {
+  type = string
+  default = "default-allow-rdp"
+}
+
+variable "default_ssh_firewall_name" {
+  type = string
+  default = "default-allow-ssh"
+}
+
+variable "bucket_names" {
+  type = list(string)
+  default = [
+    "aviato-game-fight-rvxirf.appspot.com",
+    "aviato-game-fight-rvxirf_bucket",
+    "staging.aviato-game-fight-rvxirf.appspot.com"
+  ]
+}
