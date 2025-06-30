1805c941177f187bf95407bfbfb6af8618f4b7b1
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 12:12:34 +0530

    Initial commit

---
+++ output/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  type = string
+  description = "The ID of the project"
+  default = "aviato-game-fight-rvxirf"
+}
+
+variable "region" {
+  type = string
+  description = "The region to deploy resources"
+  default = "us-central1"
+}
