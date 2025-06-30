abd5d9fe5b396d89ef56defc4af2b2ed0ae1fd3b
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:15:27 +0530

    Initial commit

--- output/compute.tf
+++ output/compute.tf
@@ -2,7 +2,10 @@ resource "google_compute_network" "default" {
   name                    = "default"
   project                 = var.project_id
   auto_create_subnetworks = false
-  delete_default_routes   = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "google_compute_firewall" "rdp" {
@@ -15,15 +18,7 @@ resource "google_compute_firewall" "rdp" {
     ports    = ["3389"]
   }
 
-  direction = "INGRESS"
-
-  source_ranges = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-  ]
-
-  target_tags = ["rdp"]
+  source_ranges = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
 }
 
 resource "google_compute_firewall" "ssh" {
@@ -36,29 +31,72 @@ resource "google_compute_firewall" "ssh" {
     ports    = ["22"]
   }
 
-  direction = "INGRESS"
+  source_ranges = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+}
 
-  source_ranges = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-  ]
-
-  target_tags = ["ssh"]
+resource "google_compute_network" "vpc_network" {
+  name                    = "custom-vpc"
+  auto_create_subnetworks = true
+  project                 = var.project_id
 }
 
 resource "google_compute_subnetwork" "default_subnetworks" {
-  for_each = toset(var.default_subnets)
+  for_each    = toset([
+    "africa-south1",
+    "southamerica-west1",
+    "us-west4",
+    "me-central2",
+    "asia-east2",
+    "asia-northeast1",
+    "asia-south2",
+    "asia-northeast3",
+    "us-south1",
+    "australia-southeast1",
+    "us-east4",
+    "us-west3",
+    "asia-southeast1",
+    "europe-southwest1",
+    "asia-east1",
+    "europe-north2",
+    "australia-southeast2",
+    "northamerica-northeast2",
+    "asia-southeast2",
+    "northamerica-northeast1",
+    "asia-south1",
+    "europe-west6",
+    "europe-west1",
+    "southamerica-east1",
+    "asia-northeast2",
+    "europe-north1",
+    "us-east5",
+    "us-west2",
+    "us-west1",
+    "europe-west10",
+    "us-central1",
+    "northamerica-south1",
+    "europe-west4",
+    "europe-west9",
+    "europe-west2",
+    "europe-west12",
+    "europe-west8",
+    "me-central1",
+    "europe-west3",
+    "us-east1",
+    "europe-central2",
+    "me-west1"
+  ])
+  name          = each.key
+  ip_cidr_range = "10.10.0.0/20"
+  network       = google_compute_network.vpc_network.id
+  region        = each.key
+  project       = var.project_id
+  flow_logs     = true
+}
 
-  name                     = each.value
-  ip_cidr_range            = "10.10.0.0/20"
-  network                  = "default"
-  private_ip_google_access = true
-  region                   = each.value
-  project                  = var.project_id
-  log_config {
-    aggregation_interval = "INTERVAL_5_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
+resource "google_compute_global_network" "default_network" {
+    name                    = "default"
+    project                 = var.project_id
+    lifecycle {
+        prevent_destroy = true
+    }
 }
