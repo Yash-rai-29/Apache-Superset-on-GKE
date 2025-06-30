3c2accb1c6b0820e865dbf780decbedc7c10dc4f
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:24:52 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -1,9 +1,16 @@
 variable "project_id" {
-  type = string
-  default = "aviato-game-fight-rvxirf"
+  type        = string
+  description = "The ID of the project"
+  default     = "aviato-game-fight-rvxirf"
 }
 
-variable "regions" {
+variable "region" {
+  type        = string
+  description = "The region to deploy resources to"
+  default     = "us-central1"
+}
+
+variable "default_subnets" {
   type = list(string)
   default = [
     "africa-south1",
@@ -44,10 +51,7 @@ variable "regions" {
     "europe-west12",
     "europe-west8",
     "me-central1",
-    "europe-west3",
-    "us-east1",
-    "europe-central2",
-    "me-west1"
+    "europe-west3"
   ]
 }
 
