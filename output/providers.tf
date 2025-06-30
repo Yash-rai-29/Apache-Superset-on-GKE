b7013e2e0a4c1658dd1f18b94a48c61cf452a317
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:18:26 +0530

    Initial commit

--- output/providers.tf
+++ output/providers.tf
@@ -2,17 +2,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.24.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "google" {
   project = var.project_id
-  region  = var.default_region
-}
-
-provider "google-beta" {
-  project = var.project_id
-  region  = var.default_region
+  region  = var.region
 }
