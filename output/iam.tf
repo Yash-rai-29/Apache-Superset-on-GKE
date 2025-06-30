77ef7f9b4fa397e65a942679c97f24b0e714aead
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 12:53:13 +0530

    Initial commit

--- output/iam.tf
+++ output/iam.tf
@@ -1,27 +1,31 @@
-resource "google_project_iam_member" "twitch_login_firebase_sdkAdminServiceAgent" {
-  provider = google
-  project = "aviato-game-fight-rvxirf"
-  role = "roles/firebase.sdkAdminServiceAgent"
-  member = "serviceAccount:twitch-login@aviato-game-fight-rvxirf.iam.gserviceaccount.com"
+data "google_project_iam_policy" "project" {
+  project = var.project_id
 }
 
-resource "google_project_iam_member" "appspot_editor" {
-  provider = google
-  project = "aviato-game-fight-rvxirf"
-  role = "roles/editor"
-  member = "serviceAccount:aviato-game-fight-rvxirf@appspot.gserviceaccount.com"
+resource "google_project_iam_member" "remove_editor_compute" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:30647320905-compute@developer.gserviceaccount.com"
+
+  lifecycle {
+    replace_triggered_by = [data.google_project_iam_policy.project]
+  }
 }
 
-resource "google_project_iam_member" "compute_editor" {
-  provider = google
-  project = "aviato-game-fight-rvxirf"
-  role = "roles/editor"
-  member = "serviceAccount:30647320905-compute@developer.gserviceaccount.com"
+resource "google_project_iam_member" "remove_editor_firebase" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:aviato-game-fight-rvxirf@appspot.gserviceaccount.com"
+
+  lifecycle {
+    replace_triggered_by = [data.google_project_iam_policy.project]
+  }
 }
 
-resource "google_project_iam_member" "firebase_adminsdk_storage_admin" {
-  provider = google
-  project = "aviato-game-fight-rvxirf"
-  role = "roles/storage.admin"
-  member = "serviceAccount:firebase-adminsdk-d21rv@aviato-game-fight-rvxirf.iam.gserviceaccount.com"
+resource "google_project_iam_binding" "service_account_binding" {
+  project = var.project_id
+  role    = "roles/firebase.sdkAdminServiceAgent"
+  members = [
+    "serviceAccount:twitch-login@aviato-game-fight-rvxirf.iam.gserviceaccount.com",
+  ]
 }
