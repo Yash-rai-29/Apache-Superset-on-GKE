5ad42450bff01fc63cfb48474af4cbdc5bbb0f4d
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 12:45:03 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   type        = string
-  description = "The GCP project ID"
+  description = "The ID of the project"
   default     = "aviato-game-fight-rvxirf"
 }
 
@@ -10,59 +10,84 @@ variable "location" {
   default     = "US"
 }
 
-variable "regions" {
+variable "default_network_regions" {
   type    = list(string)
   default = [
-    "asia-east1",
     "asia-east2",
-    "asia-northeast1",
-    "asia-northeast2",
-    "asia-northeast3",
-    "asia-south1",
-    "asia-south2",
-    "asia-southeast1",
     "asia-southeast2",
-    "australia-southeast1",
-    "australia-southeast2",
-    "europe-central2",
-    "europe-north1",
-    "europe-north2",
-    "europe-southwest1",
-    "europe-west1",
-    "europe-west2",
-    "europe-west3",
-    "europe-west4",
-    "europe-west6",
+    "us-east5",
     "europe-west8",
+    "europe-west3",
     "europe-west9",
-    "europe-west10",
-    "europe-west12",
     "me-central1",
-    "me-central2",
-    "me-west1",
-    "northamerica-northeast1",
-    "northamerica-northeast2",
+    "asia-south2",
+    "asia-northeast3",
+    "australia-southeast1",
+    "asia-south1",
     "northamerica-south1",
-    "southamerica-east1",
+    "me-west1",
+    "asia-northeast2",
+    "europe-west2",
+    "asia-northeast1",
+    "me-central2",
+    "northamerica-northeast1",
     "southamerica-west1",
-    "us-central1",
-    "us-east1",
-    "us-east4",
+    "europe-west6",
+    "australia-southeast2",
+    "europe-west12",
     "us-south1",
+    "europe-central2",
+    "europe-west4",
+    "europe-west10",
+    "asia-southeast1",
+    "asia-east1",
     "us-west1",
-    "us-west2",
+    "europe-west1",
+    "northamerica-northeast1",
+    "europe-north1",
+    "africa-south1",
+    "southamerica-east1",
+    "us-west4",
     "us-west3",
-    "us-west4"
+    "us-east4",
+    "us-central1",
+    "us-west2",
+    "europe-southwest1",
+    "us-east1"
   ]
-  description = "List of regions for subnets"
+  description = "Regions where default subnet flow logs should be enabled"
 }
 
-variable "bucket_names" {
+variable "gcs_buckets_uniform_access" {
   type = list(string)
   default = [
     "aviato-game-fight-rvxirf.appspot.com",
     "aviato-game-fight-rvxirf_bucket",
-    "staging.aviato-game-fight-rvxirf.appspot.com"
+    "staging.aviato-game-fight-rvxirf.appspot.com",
   ]
-  description = "List of bucket names"
+  description = "List of GCS bucket names for uniform bucket level access"
+}
+
+variable "default_vpc_name" {
+  type        = string
+  description = "Name of the default VPC network"
+  default     = "default"
+}
+
+variable "default_ssh_allowed_sources" {
+  type        = list(string)
+  description = "The allowed source ranges for SSH access.  Modify this to restrict SSH access to specific networks."
+  default     = []
+}
+
+variable "default_rdp_allowed_sources" {
+  type        = list(string)
+  description = "The allowed source ranges for RDP access.  Modify this to restrict RDP access to specific networks."
+  default     = []
+}
+
+variable "logging_bucket" {
+  type        = string
+  description = "The name of the GCS bucket to store logs"
+  default     = "aviato-game-fight-rvxirf-logs"
 }
