6296e5cc6a24dc8f7aee4e0d0c184871796d1212
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 12:49:37 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -4,61 +4,57 @@ variable "project_id" {
   default     = "aviato-game-fight-rvxirf"
 }
 
-variable "default_regions" {
-  type = list(string)
-  default = [
-    "asia-east2",
-    "asia-southeast2",
-    "us-east5",
-    "europe-west8",
-    "europe-west3",
-    "europe-west9",
-    "me-central1",
-    "asia-south2",
-    "asia-northeast3",
-    "australia-southeast1",
-    "asia-south1",
-    "northamerica-south1",
-    "me-west1",
-    "asia-northeast2",
-    "europe-west2",
-    "asia-northeast1",
-    "me-central2",
-    "northamerica-northeast1",
-    "southamerica-west1",
-    "europe-west6",
-    "australia-southeast2",
-    "europe-west12",
-    "us-south1",
-    "europe-central2",
-    "europe-west4",
-    "europe-west10",
-    "asia-southeast1",
-    "asia-east1",
-    "us-west1",
-    "europe-west1",
-    "northamerica-northeast1",
-    "europe-north2",
-    "africa-south1",
-    "europe-north1",
-    "southamerica-east1",
-    "us-west4",
-    "us-west3",
-    "us-east4",
-    "us-central1",
-    "us-west2",
-    "europe-southwest1",
-    "us-east1",
-  ]
-  description = "List of regions where default subnets exist"
+variable "region" {
+  type        = string
+  description = "The region to deploy resources to"
+  default     = "us-central1"
 }
 
-variable "rdp_ssh_allowed_networks" {
+variable "default_vpc_regions" {
   type = list(string)
   default = [
-    "10.0.0.0/8",
+    "asia-east1",
+    "asia-east2",
+    "asia-northeast1",
+    "asia-northeast2",
+    "asia-northeast3",
+    "asia-south1",
+    "asia-south2",
+    "asia-southeast1",
+    "asia-southeast2",
+    "australia-southeast1",
+    "australia-southeast2",
+    "europe-central2",
+    "europe-north1",
+    "europe-north2",
+    "europe-southwest1",
+    "europe-west1",
+    "europe-west10",
+    "europe-west12",
+    "europe-west2",
+    "europe-west3",
+    "europe-west4",
+    "europe-west6",
+    "europe-west8",
+    "europe-west9",
+    "me-central1",
+    "me-central2",
+    "me-west1",
+    "northamerica-northeast1",
+    "northamerica-northeast2",
+    "northamerica-south1",
+    "southamerica-east1",
+    "southamerica-west1",
+    "us-central1",
+    "us-east1",
+    "us-east4",
+    "us-south1",
+    "us-west1",
+    "us-west2",
+    "us-west3",
+    "us-west4",
   ]
-  description = "List of networks allowed to access RDP and SSH ports"
+  description = "List of regions where the default VPC needs flow logs enabled"
 }
 
 variable "bucket_names" {
@@ -68,5 +64,5 @@ variable "bucket_names" {
     "aviato-game-fight-rvxirf_bucket",
     "staging.aviato-game-fight-rvxirf.appspot.com"
   ]
-  description = "List of bucket names for which uniform bucket-level access needs to be enabled."
+  description = "List of bucket names to enable uniform bucket-level access"
 }
