d206f231eea4773c14ea0e5d2e210ecd36508600
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:28:06 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -4,14 +4,8 @@ variable "project_id" {
   default     = "aviato-game-fight-rvxirf"
 }
 
-variable "region" {
-  type        = string
-  description = "The region to deploy resources to"
-  default     = "us-central1"
-}
-
-variable "default_subnets" {
-  type = list(string)
+variable "regions" {
+  type    = list(string)
   default = [
     "africa-south1",
     "southamerica-west1",
@@ -51,8 +45,24 @@ variable "default_subnets" {
     "europe-west12",
     "europe-west8",
     "me-central1",
-    "europe-west3"
+    "europe-west3",
+    "us-east1",
+    "europe-central2",
+    "me-west1"
   ]
+  description = "List of regions"
+}
+
+variable "key_prefix" {
+  type        = string
+  description = "Prefix for generated API keys"
+  default     = "generated-api-key"
+}
+
+variable "key_rotation_interval" {
+  type        = string
+  description = "The key rotation interval in days"
+  default     = "70d"
 }
 
 variable "bucket_names" {
@@ -62,4 +72,11 @@ variable "bucket_names" {
     "aviato-game-fight-rvxirf_bucket",
     "staging.aviato-game-fight-rvxirf.appspot.com"
   ]
+  description = "List of cloud storage bucket names"
+}
+
+variable "allowed_rdp_ssh_cidr_blocks" {
+  type        = list(string)
+  default     = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  description = "List of CIDR blocks allowed for RDP and SSH access."
 }
