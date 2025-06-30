bf856bd33be48cf6e4fcc369c06fe8bd64ce7d4b
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:17:01 +0530

    Initial commit

--- output/cloud_storage_buckets.tf
+++ output/cloud_storage_buckets.tf
@@ -1,21 +1,18 @@
-resource "google_storage_bucket" "appspot_bucket" {
-  provider = google
+resource "google_storage_bucket" "default" {
   name                        = "${var.project_id}.appspot.com"
   project                     = var.project_id
   location                    = "AUSTRALIA-SOUTHEAST1"
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "aviato_bucket" {
-  provider = google
+resource "google_storage_bucket" "default2" {
   name                        = "${var.project_id}_bucket"
   project                     = var.project_id
   location                    = "US"
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "staging_bucket" {
-  provider = google
+resource "google_storage_bucket" "default3" {
   name                        = "staging.${var.project_id}.appspot.com"
   project                     = var.project_id
   location                    = "AUSTRALIA-SOUTHEAST1"
