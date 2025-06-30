e44a842a2db61738616052ea33d544b4d9e9daf2
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:12:38 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -4,53 +4,58 @@ variable "project_id" {
   default     = "aviato-game-fight-rvxirf"
 }
 
-variable "regions" {
+variable "region" {
+  type        = string
+  description = "The region to deploy resources to"
+  default     = "us-central1"
+}
+
+variable "default_subnets" {
   type = list(string)
   default = [
     "africa-south1",
-    "southamerica-west1",
-    "us-west4",
-    "me-central2",
+    "asia-east1",
     "asia-east2",
     "asia-northeast1",
-    "asia-south2",
-    "asia-northeast3",
-    "us-south1",
-    "australia-southeast1",
-    "us-east4",
-    "us-west3",
-    "asia-southeast1",
-    "europe-southwest1",
-    "asia-east1",
-    "europe-north2",
-    "australia-southeast2",
-    "northamerica-northeast2",
-    "asia-southeast2",
-    "northamerica-northeast1",
-    "asia-south1",
-    "europe-west6",
-    "europe-west1",
-    "southamerica-east1",
     "asia-northeast2",
-    "europe-north1",
-    "us-east5",
-    "us-west2",
-    "us-west1",
-    "europe-west10",
-    "us-central1",
-    "northamerica-south1",
-    "europe-west4",
-    "europe-west9",
-    "europe-west2",
-    "europe-west12",
-    "europe-west8",
-    "me-central1",
-    "europe-west3",
-    "us-east1",
+    "asia-northeast3",
+    "asia-south1",
+    "asia-south2",
+    "asia-southeast1",
+    "asia-southeast2",
+    "australia-southeast1",
+    "australia-southeast2",
     "europe-central2",
+    "europe-north1",
+    "europe-north2",
+    "europe-southwest1",
+    "europe-west1",
+    "europe-west2",
+    "europe-west3",
+    "europe-west4",
+    "europe-west6",
+    "europe-west8",
+    "europe-west9",
+    "europe-west10",
+    "europe-west12",
+    "me-central1",
+    "me-central2",
     "me-west1",
+    "northamerica-northeast1",
+    "northamerica-northeast2",
+    "northamerica-south1",
+    "southamerica-east1",
+    "southamerica-west1",
+    "us-central1",
+    "us-east1",
+    "us-east4",
+    "us-east5",
+    "us-south1",
+    "us-west1",
+    "us-west2",
+    "us-west3",
+    "us-west4"
   ]
-  description = "List of regions"
 }
 
 variable "bucket_names" {
@@ -60,5 +65,4 @@ variable "bucket_names" {
     "aviato-game-fight-rvxirf_bucket",
     "staging.aviato-game-fight-rvxirf.appspot.com"
   ]
-  description = "List of bucket names"
 }
