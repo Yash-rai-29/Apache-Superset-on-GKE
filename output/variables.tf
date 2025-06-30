33601ce57373a58d9ed36160e709e67746ed383c
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 12:44:16 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -1,33 +1,60 @@
 variable "project_id" {
   type        = string
-  description = "The ID of the project"
+  description = "The GCP project ID"
   default     = "aviato-game-fight-rvxirf"
 }
 
-variable "region" {
+variable "location" {
   type        = string
-  description = "The region to deploy resources"
-  default     = "us-central1"
+  description = "The location for resources"
+  default     = "US"
 }
 
-variable "default_ssh_source_ranges" {
-  type = list(string)
+variable "regions" {
+  type    = list(string)
   default = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16"
+    "asia-east1",
+    "asia-east2",
+    "asia-northeast1",
+    "asia-northeast2",
+    "asia-northeast3",
+    "asia-south1",
+    "asia-south2",
+    "asia-southeast1",
+    "asia-southeast2",
+    "australia-southeast1",
+    "australia-southeast2",
+    "europe-central2",
+    "europe-north1",
+    "europe-north2",
+    "europe-southwest1",
+    "europe-west1",
+    "europe-west2",
+    "europe-west3",
+    "europe-west4",
+    "europe-west6",
+    "europe-west8",
+    "europe-west9",
+    "europe-west10",
+    "europe-west12",
+    "me-central1",
+    "me-central2",
+    "me-west1",
+    "northamerica-northeast1",
+    "northamerica-northeast2",
+    "northamerica-south1",
+    "southamerica-east1",
+    "southamerica-west1",
+    "us-central1",
+    "us-east1",
+    "us-east4",
+    "us-south1",
+    "us-west1",
+    "us-west2",
+    "us-west3",
+    "us-west4"
   ]
-  description = "Source IP ranges for SSH access"
-}
-
-variable "default_rdp_source_ranges" {
-  type = list(string)
-  default = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16"
-  ]
-  description = "Source IP ranges for RDP access"
+  description = "List of regions for subnets"
 }
 
 variable "bucket_names" {
@@ -37,54 +64,5 @@ variable "bucket_names" {
     "aviato-game-fight-rvxirf_bucket",
     "staging.aviato-game-fight-rvxirf.appspot.com"
   ]
-  description = "List of bucket names to enforce uniform bucket-level access"
-}
-
-variable "default_subnet_regions" {
-  type = list(string)
-  default = [
-    "asia-east2",
-    "asia-southeast2",
-    "us-east5",
-    "europe-west8",
-    "europe-west3",
-    "europe-west9",
-    "me-central1",
-    "asia-south2",
-    "asia-northeast3",
-    "australia-southeast1",
-    "asia-south1",
-    "northamerica-south1",
-    "me-west1",
-    "asia-northeast2",
-    "europe-west2",
-    "asia-northeast1",
-    "me-central2",
-    "northamerica-northeast1",
-    "southamerica-west1",
-    "europe-west6",
-    "australia-southeast2",
-    "europe-west12",
-    "us-south1",
-    "europe-central2",
-    "europe-west4",
-    "europe-west10",
-    "asia-southeast1",
-    "asia-east1",
-    "us-west1",
-    "europe-west1",
-    "northamerica-northeast1",
-    "europe-north1",
-    "africa-south1",
-    "southamerica-east1",
-    "us-west4",
-    "us-west3",
-    "us-east4",
-    "us-central1",
-    "us-west2",
-    "europe-southwest1",
-    "us-east1",
-    "northamerica-northeast1"
-  ]
-  description = "List of regions where default subnets exist"
+  description = "List of bucket names"
 }
