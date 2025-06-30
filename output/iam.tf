73d987755a4ab9f4ce5938c378e54acdd1229ead
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:13:52 +0530

    Initial commit

--- output/iam.tf
+++ output/iam.tf
@@ -1,18 +1,44 @@
-data "google_project_iam_policy" "project" {
-  provider = google
-  project = var.project_id
+data "google_project" "project" {
+  project_id = var.project_id
 }
 
-resource "google_project_iam_member" "no_service_account_user" {
+resource "google_project_iam_binding" "service_account_token_creator" {
   provider = google
-  project = var.project_id
-  role   = "roles/iam.serviceAccountUser"
-  member = "allUsers"
+  project = data.google_project.project.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  members = []
 }
 
-resource "google_project_iam_member" "no_service_account_token_creator" {
+resource "google_project_iam_binding" "service_account_user" {
   provider = google
-  project = var.project_id
-  role   = "roles/iam.serviceAccountTokenCreator"
-  member = "allUsers"
+  project = data.google_project.project.project_id
+  role    = "roles/iam.serviceAccountUser"
+  members = []
 }
+
+resource "google_project_iam_binding" "admin" {
+  provider = google
+  project = data.google_project.project.project_id
+  role    = "roles/owner"
+  members = []
+}
+
+resource "google_service_account" "accounts" {
+  provider = google
+  account_id   = "twitch-login"
+  project      = var.project_id
+}
+
+resource "google_service_account" "accounts_compute" {
+  provider = google
+  account_id   = "compute"
+  project      = var.project_id
+}
+
+resource "google_service_account" "accounts_firebase" {
+  provider = google
+  account_id   = "firebase"
+  project      = var.project_id
+}
+
+
