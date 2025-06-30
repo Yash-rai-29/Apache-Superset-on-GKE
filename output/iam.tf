7406c88038b09a4e3dd0267c08b4b8f52064e38a
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2025-06-30 11:15:37 +0530

    Initial commit

--- output/iam.tf
+++ output/iam.tf
@@ -2,43 +2,9 @@ data "google_project" "project" {
   project_id = var.project_id
 }
 
-resource "google_project_iam_binding" "service_account_token_creator" {
-  provider = google
+resource "google_project_iam_member" "project" {
+  count  = length(data.google_project.project.project_id) > 0 ? 1 : 0
   project = data.google_project.project.project_id
-  role    = "roles/iam.serviceAccountTokenCreator"
-  members = []
+  role   = "roles/viewer"
+  member = "allUsers"
 }
-
-resource "google_project_iam_binding" "service_account_user" {
-  provider = google
-  project = data.google_project.project.project_id
-  role    = "roles/iam.serviceAccountUser"
-  members = []
-}
-
-resource "google_project_iam_binding" "admin" {
-  provider = google
-  project = data.google_project.project.project_id
-  role    = "roles/owner"
-  members = []
-}
-
-resource "google_service_account" "accounts" {
-  provider = google
-  account_id   = "twitch-login"
-  project      = var.project_id
-}
-
-resource "google_service_account" "accounts_compute" {
-  provider = google
-  account_id   = "compute"
-  project      = var.project_id
-}
-
-resource "google_service_account" "accounts_firebase" {
-  provider = google
-  account_id   = "firebase"
-  project      = var.project_id
-}
-
-
